703d33179f92b0d74e55189f73e2ead6d24d302c
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-18 15:08:08 +0200

    Created create_page_link. Updated next_page, main.

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup, Tag
 
@@ -14,19 +15,26 @@ class Quote:
     tags: list[str]
 
 
-def get_soup_page() -> BeautifulSoup:
-    r = requests.get(BASE_URL, ).content
+def create_page_link(base_url: str, next_page_url: str) -> str:
+    return urljoin(base_url, next_page_url)
+
+
+def get_soup_page(url: str) -> BeautifulSoup:
+    r = requests.get(url, ).content
     soup = BeautifulSoup(r, "html.parser")
     return soup
 
 
-def next_page(soup: BeautifulSoup) -> tuple:
+def next_page(soup: BeautifulSoup) -> dict:
     next_class = soup.select(".next")
     is_next_page = bool(len(next_class))
     next_page_link = None
     if is_next_page:
         next_page_link = soup.select(".next")[0].a.attrs["href"]
-    return is_next_page, next_page_link
+    return {
+        "is_next_page": is_next_page,
+        "next_page_link": next_page_link,
+    }
 
 
 def get_quotes(soup_page: BeautifulSoup) -> list[Tag]:
@@ -42,13 +50,23 @@ def pars_single_quote(quote: Tag) -> Quote:
 
 
 def main(output_csv_path: str) -> None:
-    bs_page = get_soup_page()
+    page_link = create_page_link(BASE_URL, "")
+    bs_page = get_soup_page(page_link)
     is_next_page = next_page(bs_page)
     quotes = get_quotes(bs_page)
-    parsed_quotes = [
-        pars_single_quote(quote)
-        for quote in quotes
-    ]
+    parsed_quotes = []
+    while True:
+        for quote in quotes:
+            parsed_quotes.append(
+                pars_single_quote(quote)
+            )
+        if is_next_page["is_next_page"]:
+            page_link = create_page_link(BASE_URL, is_next_page["next_page_link"])
+            bs_page = get_soup_page(page_link)
+            is_next_page = next_page(bs_page)
+            quotes = get_quotes(bs_page)
+        else:
+            break
 
 
 if __name__ == "__main__":
