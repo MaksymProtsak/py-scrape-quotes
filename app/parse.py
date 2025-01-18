cceec3a940361829ce63abee60904a44ad200970
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-18 12:52:38 +0200

    Created get_copu_page, next_page. Updated get_quotes, main functions.

--- app/parse.py
+++ app/parse.py
@@ -14,10 +14,23 @@ class Quote:
     tags: list[str]
 
 
-def get_quotes() -> list[Tag]:
+def get_soup_page() -> BeautifulSoup:
     r = requests.get(BASE_URL, ).content
     soup = BeautifulSoup(r, "html.parser")
-    quotes = soup.select(".quote")
+    return soup
+
+
+def next_page(soup: BeautifulSoup) -> tuple:
+    next_class = soup.select(".next")
+    is_next_page = bool(len(next_class))
+    next_page_link = None
+    if is_next_page:
+        next_page_link = soup.select(".next")[0].a.attrs["href"]
+    return is_next_page, next_page_link
+
+
+def get_quotes(soup_page: BeautifulSoup) -> list[Tag]:
+    quotes = soup_page.select(".quote")
     return quotes
 
 
@@ -29,7 +42,9 @@ def pars_single_quote(quote: Tag) -> Quote:
 
 
 def main(output_csv_path: str) -> None:
-    quotes = get_quotes()
+    bs_page = get_soup_page()
+    is_next_page = next_page(bs_page)
+    quotes = get_quotes(bs_page)
     parsed_quotes = [
         pars_single_quote(quote)
         for quote in quotes
