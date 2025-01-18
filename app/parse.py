2ffe80ff0ebb47cecf45c87e031405140be356aa
Maksym Protsak <maksym.protsak@gmail.com>
2025-01-18 12:04:35 +0200

    Created get_quotes, pars_single_quote. Updated main.

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 import requests
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -12,11 +14,26 @@ class Quote:
     tags: list[str]
 
 
+def get_quotes() -> list[Tag]:
+    r = requests.get(BASE_URL, ).content
+    soup = BeautifulSoup(r, "html.parser")
+    quotes = soup.select(".quote")
+    return quotes
+
+
+def pars_single_quote(quote: Tag) -> Quote:
+    text = quote.select(".text")[0].contents[0]
+    author = quote.select(".author")[0].contents[0]
+    tags = [str(tag.contents[0]) for tag in quote.select(".tag")]
+    return Quote(text=str(text), author=str(author), tags=list(tags))
+
+
 def main(output_csv_path: str) -> None:
-    r = requests.get("https://quotes.toscrape.com/",)
-    bs = BeautifulSoup(r.text, "html.parser")
-    bs_quotes = bs.find(".quote")
-    print(bs_quotes)
+    quotes = get_quotes()
+    parsed_quotes = [
+        pars_single_quote(quote)
+        for quote in quotes
+    ]
 
 
 if __name__ == "__main__":
